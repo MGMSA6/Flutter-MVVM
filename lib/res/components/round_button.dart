56c7430e4e63a6d5e13de2ac107b7d0580438a02
MGMSA <anand004.af@gmail.com>
2024-05-13 11:11:02 +0530

    Added Login Screen

---
+++ lib/res/components/round_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm/res/color.dart';
+import 'package:mvvm/res/strings.dart';
+
+class RoundButton extends StatelessWidget {
+  final String title;
+  final bool loading;
+  final VoidCallback onPress;
+
+  const RoundButton(
+      {super.key,
+      required this.title,
+      this.loading = false,
+      required this.onPress});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      width: 200,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: AppColours.buttonColor,
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: onPress,
+          borderRadius: BorderRadius.circular(15),
+          child: Center(
+            child: Text(
+              AppStrings.login,
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm/res/components/round_button.dart';
+import 'package:mvvm/res/strings.dart';
 import 'package:mvvm/utils/utils.dart';
 
 class LoginView extends StatefulWidget {
@@ -18,34 +20,57 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: Text("Login"),
+        ),
         body: SafeArea(
-      child: Column(
-        children: [
-          TextFormField(
-            controller: emailController,
-            keyboardType: TextInputType.emailAddress,
-            focusNode: emailFocus,
-            decoration: InputDecoration(
-                hintText: 'Email',
-                labelText: 'Email',
-                prefixIcon: Icon(Icons.alternate_email)),
-            onFieldSubmitted: (value) {
-              Utils.fieldFocusNode(context, emailFocus, passwordFocus);
-            },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                focusNode: emailFocus,
+                decoration: InputDecoration(
+                    hintText: 'Email',
+                    labelText: 'Email',
+                    prefixIcon: Icon(Icons.alternate_email)),
+                onFieldSubmitted: (value) {
+                  Utils.fieldFocusNode(context, emailFocus, passwordFocus);
+                },
+              ),
+              TextFormField(
+                obscureText: true,
+                controller: passwordController,
+                keyboardType: TextInputType.emailAddress,
+                focusNode: passwordFocus,
+                decoration: InputDecoration(
+                    hintText: 'Password',
+                    labelText: 'Password',
+                    prefixIcon: Icon(Icons.lock_open_rounded),
+                    suffixIcon: Icon(Icons.visibility_off_rounded)),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              RoundButton(
+                  title: AppStrings.login,
+                  onPress: () {
+                    if (emailController.text.toString().isEmpty) {
+                      Utils.flushBarErrorMessage("Please enter email", context);
+                    } else if (passwordController.text.toString().isEmpty) {
+                      Utils.flushBarErrorMessage(
+                          "Please enter password", context);
+                    } else if (passwordController.text.length < 6) {
+                      Utils.flushBarErrorMessage(
+                          "Please enter 6 digit password", context);
+                    } else {
+                      print("Api Hit");
+                    }
+                  }),
+            ],
           ),
-          TextFormField(
-            obscureText: true,
-            controller: passwordController,
-            keyboardType: TextInputType.emailAddress,
-            focusNode: passwordFocus,
-            decoration: InputDecoration(
-                hintText: 'Password',
-                labelText: 'Password',
-                prefixIcon: Icon(Icons.lock_open_rounded),
-                suffixIcon: Icon(Icons.visibility_off_rounded)),
-          )
-        ],
-      ),
-    ));
+        ));
   }
 }
